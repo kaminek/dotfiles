14cdafb50f9e65cfc2e8b16da7bcbb32fc9b8115
Amine Kherbouche <kaminek92@gmail.com>
2017-10-15 00:00:08 +0200

    i3: add config file

---
+++ .i3/lock.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LOCK_ICON=~/.icons/lock1.png
+LOCK_SCREEN=/tmp/screen.png
+scrot $LOCK_SCREEN
+convert $LOCK_SCREEN -scale 10% -scale 1000% $LOCK_SCREEN
+[[ -f $1 ]] && convert $LOCK_SCREEN $1 -gravity center -composite -matte $LOCK_SCREEN
+i3lock -u -i $LOCK_SCREEN
+rm $LOCK_SCREEN
