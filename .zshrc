kaminek/dotfiles

commit cccbaa50d950beeedf53692058fe53f3f2baf123

zshrc: source and export fzf stuff

.zshrc
@@ -312,8 +312,25 @@ man() {
 [[ -f $ZSH/oh-my-zsh.sh ]] && source $ZSH/oh-my-zsh.sh
 [[ -f $HOME/.aliases ]] && source $HOME/.aliases
 
+# fzf
+if [ -e /usr/local/opt/fzf/shell/completion.zsh ]; then
+  source /usr/local/opt/fzf/shell/key-bindings.zsh
+  source /usr/local/opt/fzf/shell/completion.zsh
+fi
+if [ -e ~/.fzf ]; then
+  _append_to_path ~/.fzf/bin
+  source ~/.fzf/shell/key-bindings.zsh
+  source ~/.fzf/shell/completion.zsh
+fi
+if _has fzf && _has ag; then
+  export FZF_DEFAULT_COMMAND='ag --nocolor -g ""'
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
+  export FZF_DEFAULT_OPTS='
+  --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
+  --color info:108,prompt:109,spinner:108,pointer:168,marker:168
+  '
+fi
+
 # Call upon launch
 screenfetch
-
-# Use fzf for lookup
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh

commit 4bc3d3c3701b53c1733caf34bb995e29c9b2a07d

zshrc: add path utility functions
Fixes: cccbaa50d950 ("zshrc: source and export fzf stuff")

.zshrc
@@ -32,6 +32,17 @@ _try() {
   return $( eval $* >/dev/null 2>&1 )
 }
 
+_prepend_to_path() {
+  if [ -d $1 ]; then
+    export PATH="$PATH:$1" ;
+  fi
+}
+
+_append_to_path() {
+  if [ -d $1 ]; then
+    export PATH="$1:$PATH" ;
+  fi
+}
 # Enviroment variables
 
 export DEFAULT_USER=$USER